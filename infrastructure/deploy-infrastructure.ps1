e1f159f07185dcc7597e21ca7c14b1dc318410ab
TNgonephetsy-insight <116840552+TNgonephetsy-insight@users.noreply.github.com>
2022-12-02 18:05:12 +0000

    commit to merge workingmain

---
+++ infrastructure/deploy-infrastructure.ps1
@@ -0,0 +1,15 @@
+$studentprefix = "trn"
+$resourcegroupName = "fabmedical-rg-" + $studentprefix
+$cosmosDBName = "fabmedical-cdb-" + $studentprefix
+$webappName = "fabmedical-web-" + $studentprefix
+$planName = "fabmedical-plan-" + $studentprefix
+$location1 = "westeurope"
+$location2 = "northeurope"
+az group create -l $location1 -n $resourcegroupName
+#CosmoDB creation
+az cosmosdb create --name $cosmosDBName `
+--resource-group $resourcegroupName `
+--locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
+--locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
+--enable-multiple-write-locations `
+--kind MongoDB 
