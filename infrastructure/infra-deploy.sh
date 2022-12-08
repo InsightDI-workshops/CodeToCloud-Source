eb3ff7f651d37d50790f09e2099c2bb16ced537a
AndrewSutliff-insight <andrew.sutliff@insight.com>
2022-12-08 15:26:35 -0500

    added auto-retrieve database string

--- infrastructure/infra-deploy.sh
+++ infrastructure/infra-deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 GH_ACCESS_TOKEN="$1"
-MONGODB_CONNECTION="$2"
+MONGODB_CONNECTION=$(az cosmosdb keys list -n fabrikam-cdb-aes  -g fabrikam-rg-aes --type connection-strings \
+--query "connectionStrings[?description=='Primary MongoDB Connection String'].connectionString" | tr -d '\n',' ','[',']','\"')
+#still need to add name of db in the connection string
 SUFFIX="aes"
 RESOURCE_GROUP_NAME="fabrikam-rg-"$SUFFIX
 WEBAPP_NAME="fabrikam-webapp-"$SUFFIX
-# add content db after the port number
 docker run -it --rm -e MONGODB_CONNECTION=$MONGODB_CONNECTION ghcr.io/andrewsutliff-insight/fabrikam-init
-az webapp config appsettings set -n $WEBAPP_NAME -g $RESOURCE_GROUP_NAME --settings MONGODB_CONNECTION=$MONGODB_CONNECTION --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
+az webapp config appsettings set -n $WEBAPP_NAME -g $RESOURCE_GROUP_NAME --settings MONGODB_CONNECTION=$MONGODB_CONNECTION WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
+#mongodb connection string should be found after running the infra-init script
 az webapp config container set \
     --docker-registry-server-password $GH_ACCESS_TOKEN \
     --docker-registry-server-url https://ghcr.io \
