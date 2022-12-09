6956f6c9cf37e285302f39247399f05d41d29dd7
AndrewSutliff-insight <andrew.sutliff@insight.com>
2022-12-09 15:54:34 -0500

    Added Application Insights

--- infrastructure/app-insights.sh
+++ infrastructure/app-insights.sh
@@ -5,5 +5,23 @@ APP_INSIGHTS="fabrikamai-"$SUFFIX
 
 az extension add --name application-insights
 AI=$(az monitor app-insights component create --app $APP_INSIGHTS --location $LOCATION1 --kind web -g $RESOURCE_GROUP_NAME --application-type web --retention-time 120 -o json)
+AI_KEY=$(echo $AI | jq -r '.instrumentationKey')
+echo $AI_KEY
 
-echo $(echo $AI | jq -r '.instrumentationKey')
+sed -i -e "s/^appInsights.setup.*/appInsights\.setup(\"${AI_KEY}\");/" ./content-web/app.js
+
+git add .
+git commit -m "Added Application Insights"
+git push
+
+sleep 200
+
+az webapp config container set \
+    --docker-registry-server-password $GH_ACCESS_TOKEN \
+    --docker-registry-server-url https://ghcr.io \
+    --docker-registry-server-user notapplicable \
+    --multicontainer-config-file docker-compose.yml \
+    --multicontainer-config-type COMPOSE \
+    --name $WEBAPP_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --enable-app-service-storage true
