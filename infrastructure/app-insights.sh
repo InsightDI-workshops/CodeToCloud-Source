acc67e70d373813e835680a0407a4f5702e73bfd
AndrewSutliff-insight <andrew.sutliff@insight.com>
2022-12-12 11:21:18 -0500

    new settings config for app insights on webapp

--- infrastructure/app-insights.sh
+++ infrastructure/app-insights.sh
@@ -21,6 +21,19 @@ git push
 
 sleep 250
 
+az webapp config appsettings set -n $WEBAPP_NAME -g $RESOURCE_GROUP_NAME \
+--settings APPINSIGHTS_INSTRUMENTATIONKEY $AI_KEY \
+    --APPINSIGHTS_PROFILEFEATURE_VERSION=1.0.0 \
+    --APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 \
+    --APPLICATIONINSIGHTS_CONNECTION_STRING=$AI_CONNECTION \
+    --ApplicationInsightsAgent_EXTENSION_VERSION=~2 \
+    --DiagnosticServices_EXTENSION_VERSION=~3 \
+    --InstrumentationEngine_EXTENSION_VERSION=disabled \
+    --SnapshotDebugger_EXTENSION_VERSION=disabled \
+    --XDT_MicrosoftApplicationInsights_BaseExtensions=disabled \
+    --XDT_MicrosoftApplicationInsights_Mode=recommended \
+    --XDT_MicrosoftApplicationInsights_PreemptSdk=disabled
+
 az webapp config container set \
     --docker-registry-server-password $GH_ACCESS_TOKEN \
     --docker-registry-server-url https://ghcr.io \
@@ -30,14 +43,3 @@ az webapp config container set \
     --name $WEBAPP_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
     --enable-app-service-storage true \
-    --appinsights-instrumentationkey $AI_KEY \
-    --appinsights-profilefeature-version 1.0.0 \
-    --appinsights-snapshotfeature-version 1.0.0 \
-    --applicationinsights-connection-string $AI_CONNECTION \
-    --applicationinsightsagent-extension-version ~2 \
-    --daignostics-services-extension-version ~3 \
-    --instrumentation-engine-extension-version disabled \
-    --snapshotdebugger-extension-version disabled \
-    --xdt-microsoft-applicationinsights-base-extensions disabled \
-    --xdt-microsoft-applicationinsights-mode recommended \
-    --xdt-microsoft-applicationinsights-preempt-sdk disabled
